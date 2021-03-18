c5233ce539735f733c5d208c2c8779ee947b71cc
Lage, Mateus Mamede <mlage@bethel.jw.org>
2021-03-18 16:47:40 -0400

    New styles

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,15 +1,20 @@
+import 'dotenv/config'
 import ghpages from 'gh-pages'
 import { build } from './common'
+import pkg from '../package.json'
 
 process.env.NODE_ENV = 'production'
+const repo = pkg.repository.url.replace(
+  /(https:\/\/)/,
+  `$1${process.env.GH_TOKEN}@`
+)
 
 deploy()
 
 async function deploy() {
   await build()
-  console.log('test')
 
-  ghpages.publish('build', (err) => {
+  ghpages.publish('build', { repo }, (err) => {
     err && console.error(err)
   })
 }
--- src/components/app/app-view.tsx
+++ src/components/app/app-view.tsx
@@ -1,36 +1,41 @@
 /** @jsx jsx */
-import { css, jsx } from "@emotion/react";
+import { css, jsx } from '@emotion/react'
 
-const fontCss = css`
+//#region Styles
+const containerCss = css`
   font-family: Arial, Helvetica, sans-serif;
   text-align: center;
-`;
-
+`
+const timeCodeCss = css`
+  font-size: 16vw;
+  font-weight: bold;
+`
+const buttonSize = 20
 const buttonCss = css`
-  width: 60px;
-  height: 60px;
-  border-radius: 30px;
-`;
-
+  width: ${buttonSize}vw;
+  height: ${buttonSize}vw;
+  border-radius: ${buttonSize / 2}vw;
+  font-size: ${buttonSize / 5}vw;
+`
 const playCss = css`
   background: green;
-`;
-
+`
+//#endregion
 export interface AppViewProps {
-  timeLabel: string;
-  isPaused: boolean;
-  togglePaused: () => void;
-  reset: () => void;
+  timeLabel: string
+  isPaused: boolean
+  togglePaused: () => void
+  reset: () => void
 }
 
 export const AppView = (props: AppViewProps) => (
-  <div css={fontCss}>
-    <h1>{props.timeLabel}</h1>
+  <div css={containerCss}>
+    <div css={timeCodeCss}>{props.timeLabel}</div>
     <button css={buttonCss} onClick={props.reset} disabled={!props.isPaused}>
       Reset
     </button>
     <button css={[buttonCss, playCss]} onClick={props.togglePaused}>
-      {props.isPaused ? "Start" : "Pause"}
+      {props.isPaused ? 'Start' : 'Pause'}
     </button>
   </div>
-);
+)
